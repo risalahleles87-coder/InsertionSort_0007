65fea782ae4966f9701993bc8c5170a989be3124
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-12 15:07:26 +0700

    Proses pergeseran elemen array jika lebih besar dari temp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -42,4 +42,16 @@ void insertionsort()
     {
         temp = arr[i];
         j = i - 1;
-        
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nstep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
