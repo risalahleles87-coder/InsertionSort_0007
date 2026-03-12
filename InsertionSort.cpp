97a78c3e604b2ea1df4c21d37a96e2c1cb3c8d57
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-12 15:08:32 +0700

    Membuat fungsi display untuk menampilkan hasil array yang sudah diurutkan

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -55,3 +55,23 @@ void insertionsort()
         }
     }
 }
+void display()
+{
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+int main()
+{
+    input();
+    insertionsort();
+    display();
+    system("pause");
+}
